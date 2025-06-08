ab3295020641d49714c59b91df46fe6ba026e55b
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-08 14:23:06 -0700

    Added terms and privacy page and changed hr styling

---
+++ test/includes/terms-and-privacy.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Terms & Privacy</title>
+  <link rel="stylesheet" href="styles/base.css" />
+</head>
+<body>
+    <h2 style="text-decoration: underline">
+              Terms of Service & Privacy Policy
+            </h2>
+            <br />
+            <section class="terms-of-service">
+              <h3 style="text-decoration: underline">Terms of Service</h3>
+              <p>
+                By using this site, you agree to act respectfully and follow all
+                applicable laws. We reserve the right to update or remove
+                content and access as needed. This site is for personal and fan
+                use only.
+              </p>
+            </section>
+            <br />
+            <section class="privacy-policy">
+              <h3 style="text-decoration: underline">Privacy Policy</h3>
+              <p>
+                We collect basic information like usernames and favorite songs
+                only for profile personalization. This data is never sold or
+                shared. Cookies may be used to improve site functionality. You
+                can contact us if you’d like any of your data removed.
+              </p>
+            </section>
+
+</body>
+</html>
